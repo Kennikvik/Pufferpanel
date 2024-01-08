29735d8902f892454f0d6d909318e33d3eae5cf1
Kenni Thomasberg <kennikvik@gmail.com>
2024-01-08 18:35:29 +0000

    FIX docker locations and symbolic links to /var/lib/pufferpanel/*

--- install.sh
+++ install.sh
@@ -5,4 +5,15 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-mkdir -p /data/pufferpanel
+## Create primary data hostpaths
+mkdir -p /data/pufferpanel/servers
+mkdir -p /data/pufferpanel/binaries
+mkdir -p /data/pufferpanel/logs
+mkdir -p /data/pufferpanel/config
+mkdir -p /data/pufferpanel/modules
+
+
+ln -s /data/pufferpanel/servers /var/lib/servers
+ln -s /data/pufferpanel/binaries /var/lib/binaries
+
+
